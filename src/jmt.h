464c456d8a117b4164c8af70759472dc86ea1f0e
Srikanth Narayanan <srikanthnarayanan@Srikanths-MacBook-Pro.local>
2017-12-10 10:17:17 -0800

    jet header file

---
+++ src/jmt.h
@@ -0,0 +1,18 @@
+//
+//  jmt.h
+//  Path_Planning
+//
+//  Created by Srikanth Narayanan on 12/9/17.
+//
+
+#ifndef JMT
+#define JMT
+
+#include <vector>
+
+using namespace std;
+
+vector<double> get_jmt_coeffs(vector<double> start, vector<double> end, double T);
+
+
+#endif /* JMT */
